5916d8a336ce6e17acfbf1396f40e8fcb0012405
mszauer <mszauer@mail.usf.edu>
2016-02-21 17:42:34 -0500

    added working screen with code

---
+++ Assets/Scripts/ScreenChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenChanger : MonoBehaviour {
+    public GameObject StartScreen = null;
+    public GameObject TimerScene = null;
+    public TimerScreen TimerScreen = null;
+    public Text totalTimeSec;
+    public Text totalTimeMin;
+
+    public void Awake() {
+        StartScreen.SetActive(true);
+        TimerScene.SetActive(false);
+    }
+    public void StartClicked() {
+        StartScreen.SetActive(false);
+        TimerScene.SetActive(true);
+        int totalSeconds = System.Convert.ToInt32(totalTimeSec.text) + (System.Convert.ToInt32(totalTimeMin.text) * 60);
+        TimerScreen.TimeFinished = System.DateTime.UtcNow.AddSeconds(totalSeconds);
+    }
+}
---
+++ Assets/Scripts/TimerScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScreen : MonoBehaviour {
+    public Text TotalTime = null;
+    public int Rounds {
+        get; set;
+    }
+    public int Work { get; set; }
+    public System.DateTime TimeFinished { get; set; }
+    public void Update() {
+        int DisplaySeconds = (TimeFinished - System.DateTime.UtcNow).Seconds;
+        int DisplayMinutes = (TimeFinished - System.DateTime.UtcNow).Minutes;
+        TotalTime.text = DisplayMinutes.ToString("D2") +" : "+ DisplaySeconds.ToString("D2");
+    }
+}
